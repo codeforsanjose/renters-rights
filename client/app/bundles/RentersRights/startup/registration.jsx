dfac93b6d58a27c2d7801363ac567a714a8f7961
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-08-01 17:33:20 -0700

    add Eviction resource message page

--- client/app/bundles/RentersRights/startup/registration.jsx
+++ client/app/bundles/RentersRights/startup/registration.jsx
@@ -1,9 +1,11 @@
+import EvictionResourcesMessage from '../components/EvictionResourcesMessage';
 import NotApplicableMessage from '../components/NotApplicableMessage';
 import ReactOnRails from 'react-on-rails';
 import Renters from '../components/Renters';
 
 // This is how react_on_rails can see the components in the browser.
 ReactOnRails.register({
+  EvictionResourcesMessage,
   NotApplicableMessage,
   Renters,
 });
