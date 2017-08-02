359b00fac90c60ced0b69bed6d9566bfce744353
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-08-01 21:00:13 -0700

    separate forms

--- client/app/bundles/RentersRights/components/Renters.jsx
+++ client/app/bundles/RentersRights/components/Renters.jsx
@@ -1,13 +1,12 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import RentersHeader from './RentersHeader'
+import RentersFooter from './RentersFooter'
 
 export default class Renters extends React.Component {
   render() {
     return (
       <div className="renters-container">
         <div className="renters-dashboard">
-          <RentersHeader />
 
           <div className="dashboard-container">
             <div className="dashboard-title">
@@ -28,16 +27,20 @@ export default class Renters extends React.Component {
                   <input type="aptOrUnit" className="form-control"/>
                 </div>
                 <br/>
-                <div className="col-xs-6">
-                  <div className="form-group">
-                    <label for="city">City:</label>
-                    <input type="text" className="form-control"/>
-                  </div>
-                </div>
-                <div className="col-xs-3">
-                  <div className="form-group">
-                  <label for="state">State:</label>
-                    <input type="text" className="form-control"/>
+                <div className="form-group">
+                  <div className="row">
+                    <div className="col-xs-6">
+
+                        <label for="city">City:</label>
+                        <input type="text" className="form-control"/>
+
+                    </div>
+                    <div className="col-xs-3">
+
+                      <label for="state">State:</label>
+                        <input type="text" className="form-control"/>
+
+                    </div>
                   </div>
                 </div>
                 <br/>
@@ -47,100 +50,21 @@ export default class Renters extends React.Component {
                     <input type="text" className="form-control"/>
                   </div>
                 </div>
-                <br/>
-                <h3>How can we help you?</h3>
-                <div className="checkbox">
-                  <label>
-                    <input type="checkbox"/> My landlord is evicting me.
-                  </label>
-                  <br/>
-                  <label>
-                    <input type="checkbox"/> My landlord refuses to fix something.
-                  </label>
-                  <br/>
-                  <label>
-                    <input type="checkbox"/> My landlord is raising my rent.
-                  </label>
-                </div>
 
-                <br/>
-                <h3>Did your landlord give you reasons for evicting you?</h3>
-                <div className="checkbox">
-                  <label>
-                    <input type="checkbox"/> Yes
-                  </label>
-                  <br/>
-                  <label>
-                    <input type="checkbox"/> No
-                  </label>
-                  <br/>
-                </div>
-
-                <br/>
-                <h3>What were their reasons?</h3>
-                <h5>Check all that apply</h5>
-                <div className="checkbox">
-                  <label>
-                    <input type="checkbox"/> Non-payment of rent
-                  </label>
-                  <br/>
-                  <label>
-                    <input type="checkbox"/> Violation of lease
-                  </label>
-                  <br/>
-                  <label>
-                    <input type="checkbox"/> Damage to the apartment
-                  </label>
-                  <br/>
-                  <label>
-                    <input type="checkbox"/> Refusal to agree to similar or new rental agreement
-                  </label>
-                  <br/>
-                  <label>
-                    <input type="checkbox"/> Nuisance behavior
-                  </label>
-                  <br/>
-                  <label>
-                    <input type="checkbox"/> Refusing access to unit
-                  </label>
-                  <br/>
-                  <label>
-                    <input type="checkbox"/> Illegally sub-leasing to an unapproved tenant
-                  </label>
-                  <br/>
-                  <label>
-                    <input type="checkbox"/> Substantial rehabilitation of the apartment
-                  </label>
-                  <br/>
-                  <label>
-                    <input type="checkbox"/> Ellis Act removal
-                  </label>
-                  <br/>
-                  <label>
-                    <input type="checkbox"/> Owner move-in
-                  </label>
-                  <br/>
-                  <label>
-                    <input type="checkbox"/> Order to vacate
-                  </label>
-                  <br/>
-                  <label>
-                    <input type="checkbox"/> Vacation of unpermitted unit
-                  </label>
-                </div>
 
                 <button
                   className="btn btn-primary"
                   onClick={this.handleClick}
                   type="submit"
                 >
-                  Submit
+                  Get Started
                 </button>
               </form>
 
             </div>
 
           </div>
+          <RentersFooter />
         </div>
       </div>
     )
