dd5d64a81f9b896040bd8185673706abdb6b59ea
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-08-01 21:45:42 -0700

    add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   # reference: http://guides.rubyonrails.org/routing.html
 
   get 'not-applicable', to: 'renters_rights#not_applicable'
+  get 'renters-reasons-form', to: 'renters_rights#renters_reasons_form'
+  get 'tenant-reallocation', to: 'renters_rights#tenant_reallocation'
+  get 'eviction-resources', to: 'renters_rights#eviction_resources'
 end
