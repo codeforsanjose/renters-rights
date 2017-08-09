05415466018e3421bd0c22395c18f997912548a6
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-08-08 23:23:09 -0700

    home layout

---
+++ client/app/bundles/RentersRights/components/Home.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import RentersLayout from './RentersLayout'
+
+export default class Home extends React.Component {
+  render() {
+    const { } = this.props;
+
+    return (
+      <RentersLayout>
+        <br/>
+        <div className="page-header"> 
+          <h1>Helping renters in San Jose understand the laws and programs in place to protect them.
+          </h1>
+        </div>
+        <br/>
+        <div className="home-links">
+        <div className="col-sm-12 col-md-7">
+        <h4> How can we help you today? </h4>
+        <div className="list-group">
+          <a href="#" className="list-group-item">
+            <h4 className="list-group-item-heading">I want to know whether I qualify for any housing programs.</h4>
+          </a>
+        </div>
+        <div className="list-group">
+          <a href="#" className="list-group-item">
+            <h4 className="list-group-item-heading">My landlord raised the rent and I cannot afford it anymore.</h4>
+          </a>
+        </div>
+        <div className="list-group">
+          <a href="/renters" className="list-group-item">
+            <h4 className="list-group-item-heading">My landlord is trying to evict me and I don't know what to do.</h4>
+          </a>
+        </div>
+        </div>
+        </div>
+      </RentersLayout>      
+    )
+  }
+}
