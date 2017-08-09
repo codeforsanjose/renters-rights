c1e9ab86ba58d012ff16583e209b6252f8747c05
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-08-08 23:57:32 -0700

    update styling to reflect boostrap version

--- client/app/bundles/RentersRights/components/Renters.jsx
+++ client/app/bundles/RentersRights/components/Renters.jsx
@@ -9,37 +9,40 @@ export default class Renters extends React.Component {
       <RentersLayout>
         <div className="dashboard-container">
           <div className="dashboard-text">
-            <p> Get started by completing the form below </p>
+            <p> Sorry to hear that your landlord is trying to evict you. Let's figure out whether it was legal.</p>
             <form
               className="form-horizontal"
               method="post"
             >
               <h3> What is your address?</h3>
-              <div className="form-group">
-                <label htmlFor="streetAddress">Street Address:</label>
-                <input type="streetAddress" className="form-control"/>
-              </div>
-              <br/>
-              <div className="form-group">
-                <label htmlFor="aptOrUnit">Apt/Unit:</label>
-                <input type="aptOrUnit" className="form-control"/>
-              </div>
-              <br/>
               <div className="form-group row">
-                <label className="col-1 col-form-label" htmlFor="city">City:</label>
-                <div className="col-4">
+                <label className="col-md-1 col-form-label" htmlFor="streetAddress">Street Address:</label>
+                <div className="col-md-8">
+                  <input 
+                    className="form-control" 
+                    type="streetAddress"
+                  />
+                </div>
+                <label className="col-md-1 col-form-label" htmlFor="aptOrUnit">Apt/Unit:</label>
+                <div className="col-md-2">
+                  <input type="aptOrUnit" className="form-control"/>
+                </div>
+              </div>
+              <div className="form-group row">
+                <label className="col-md-1 col-form-label" htmlFor="city">City:</label>
+                <div className="col-md-4">
                   <input
                     className="form-control"
                     name="city"
                     type="text"
                   />
                 </div>
-                <label className="col-1 col-form-label" htmlFor="state">State:</label>
-                <div className="col-2">
+                <label className="col-md-1 col-form-label" htmlFor="state">State:</label>
+                <div className="col-md-2">
                   <input type="text" className="form-control"/>
                 </div>
-                <label className="col-1 col-form-label" htmlFor="zip">Zip:</label>
-                <div className="col-2">
+                <label className="col-md-1 col-form-label" htmlFor="zip">Zip:</label>
+                <div className="col-md-3">
                   <input type="text" className="form-control"/>
                 </div>
               </div>
