f971bcec914833d2cb86a8d5b3b637416ba3431b
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-08-08 23:22:41 -0700

    register home

--- client/app/bundles/RentersRights/startup/registration.jsx
+++ client/app/bundles/RentersRights/startup/registration.jsx
@@ -5,9 +5,11 @@ import HelpTypeForm from '../components/HelpTypeForm';
 import NotApplicableMessage from '../components/NotApplicableMessage';
 import ReactOnRails from 'react-on-rails';
 import Renters from '../components/Renters';
+import Home from '../components/Home';
 
 // This is how react_on_rails can see the components in the browser.
 ReactOnRails.register({
+  Home,
   EvictionResourcesMessage,
   HelpTypeForm,
   NotApplicableMessage,
