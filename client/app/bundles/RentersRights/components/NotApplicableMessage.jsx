317046f5220118c189387b12b6e7a999c9d69b82
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-08-10 16:15:08 -0700

    increase font size

--- client/app/bundles/RentersRights/components/NotApplicableMessage.jsx
+++ client/app/bundles/RentersRights/components/NotApplicableMessage.jsx
@@ -8,18 +8,21 @@ export default function NotApplicableMessage({
     <RentersLayout
       className={className}
     >
-      <div className="container">
-        <strong>
-          We're sorry, but we don't cover homes outside San Jose yet.
-        </strong>
-        <p>
-          Please review our resource guide and consider following us on social media to stay updated on further developments.
-        </p>
-        <p>
-          <a href="/">
-            &laquo; Back to home page
-          </a>
-        </p>
+      <div className="home-links">
+        <div className="container">
+          <strong>
+            <h4>We're sorry, but we don't cover homes outside San José yet.</h4>
+          </strong>
+          <p>
+            <h4>Please review our <a href="/resources">resource guide</a> and consider following us on <a href="https://twitter.com/codeforsanjose">Twitter</a> to stay updated on further developments.</h4>
+          </p>
+          <br/>
+          <p>
+            <a href="/">
+              &laquo; Back to home page
+            </a>
+          </p>
+        </div>
       </div>
     </RentersLayout>
   );
