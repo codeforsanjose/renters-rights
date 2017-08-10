528174c309f62b11584ef6d1ba9af5da3c6d10cb
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-08-10 16:09:09 -0700

    update FAQ

--- client/app/bundles/RentersRights/components/FAQ.jsx
+++ client/app/bundles/RentersRights/components/FAQ.jsx
@@ -15,21 +15,42 @@ export default class FAQ extends React.Component {
         <br/>
         <div className="home-links">
           <div className="container">
-            <h4> Who is behind this app? </h4>
+            <h3> Who is behind this app? </h3>
             <p>Renter's Rights Guide is an open source web application built by Code for San José developers and designers to inform renters about their rights and resources. To learn more about the project visit our <a href="https://github.com/codeforsanjose/renters-rights/">Github repo</a>.</p>
-
-            <h4> When can a tenant be evicted? Can a tenant be evicted for no cause? </h4>
+            <br/>
+            <h3> What area does this app cover?</h3>
+            <p> Currently this application covers the City of San José. In the future, we hope to expand to other cities in Santa Clara County. </p>
+            <br/>
+            <h3> When can a tenant be evicted? Can a tenant be evicted for no cause? </h3>
             <p>On May 9, 2017, the City Council adopted the <strong>Tenant Protection Ordinance</strong>.  Additionally, the City Council adopted an Urgency Ordinance making the new regulations effective immediately. The Tenant Protection Ordinance outlines landlord responsibilities and tenant rights regarding notices to terminate.  Under the Tenant Protection Ordinance, landlords of multifamily dwellings, guesthouses and unpermitted units will only be authorized to terminate tenancy under one of the 12 listed <strong>just causes </strong> (view flyer in <a href="http://www.sanjoseca.gov/DocumentCenter/View/69126">English</a>, <a href="http://www.sanjoseca.gov/DocumentCenter/View/69112">Spanish</a> and <a href="http://www.sanjoseca.gov/DocumentCenter/View/69113">Vietnamese</a>). </p>
             <p>To learn more about the <strong>Tenant Protection Ordinance</strong>:</p>
             <ul>
               <li><a href="http://www.sanjoseca.gov/DocumentCenter/View/70042">Tenant Protection Ordinance Fact Sheet</a></li>
               <li><a href="http://www.sanjoseca.gov/DocumentCenter/View/70490">Tenant Protection Ordinance Frequently Asked Questions</a></li>
             </ul>
-
-            <h4> Is my apartment covered under rent control? How much can my rent be raised? What can I submit a petition for?</h4>
+            <br/>
+            <h3>What is the Apartment Rental Dispute Mediation and Arbitration Ordinance (ARO)?</h3>
             <p>The City of San José’s <a href="http://www.sanjoseca.gov/index.aspx?nid=1355">Apartment Rental Dispute Mediation and Arbitration Ordinance (ARO)</a> regulates the annual amount that rents can increase in apartments in San José with three or more units that were first rented before September 7, 1979. </p>
+            <br/>
+            <h4> Is my apartment covered under rent control? </h4>
+            <p>Properties that are covered by San José's Apartment Rent Ordinance include apartments with three or more units that were built and occupied prior to September 7, 1979. </p>
+            <p>Rental housing developments exempted from the ordinance include single family homes,in-law units/granny flats/accessory dwelling units, duplexes, condominiums, townhomes, hotels, boarding houses rented to transient guests for periods of less than 30 days, nonprofit homes for the aged, school dormitories, rental units owned and operated by any government agency, any new rental units first rented after September 7, 1979, and properties in unincorporated areas of San José. Learn more by visiting the <a href="http://www.sanjoseca.gov/index.aspx?nid=1355">City of San José's website</a>.</p>
+            <p>If you have an inquiry about a property outside of San José, please check with that specific city for more information about any regulations that may apply.</p>
+            <br/>
+            <h4> How much can my rent be raised?</h4>
+            <p><strong>Allowable Rent Increase Under the Ordinance</strong></p>
+            <p>Under the Apartment Rent Ordinance, rent increases may only be given once in a 12-month period. The maximum annual allowable increase is 5%.</p>
+            <p>The City Council has directed that changes be made to the Apartment Rent Ordinance. Staff anticipates that revisions to the Ordinance will be completed by the end of 2017.</p>
+            <p>Beginning June 17, 2016, an <a href="http://www.sanjoseca.gov/DocumentCenter/View/61857">Interim Ordinance</a> will be in effect until revisions to the Apartment Rent Ordinance are completed. The Interim Ordinance reduces the annual allowable rent increase from 8% to 5% and outlines the process for filing cost pass-through petitions.</p>
 
-            <h4> How can I learn more about meditation programs in the City of San José and Santa Clara County?</h4>
+            <p><strong>Exceptions to the Allowable Rent Increases</strong></p>
+            <p>Rent increases that are exempt from these requirements include an increase after the rental unit has been voluntarily vacated by the tenant, and an increase after eviction of a tenant.</p> 
+            <p>Under the Apartment Rent Ordinance, both property owners and tenants may file a petition. Property owners who file a petition to increase rent in excess 5% must submit a cost worksheet to the Rental Rights and Referrals Program. Pending a decision by the Hearing Officer, the amount of rent in excess of 5% is deferred and only an 5% rent increase should be demanded or paid.</p>
+            <p>Landlords may file a <strong>Fair Return petition</strong> if they would like to request the ability to iincrease rent more than 5% because of substantial capital improvements or capital expenses, unusually low rents, uninsured damage or vandalism, or other reasons.</p>
+            <p>Tenants with a rent increase subject to review and/or service reduction claims or housing code violations may file a petition. </p> 
+            <p>View the <a href="http://www.sanjoseca.gov/index.aspx?nid=1356">petitions</a>.</p>
+            <br/>
+            <h3> How can I learn more about meditation programs in the City of San José and Santa Clara County?</h3>
             <p>Mediation is one of the alternatives for resolving differences in a fair and equitable way. The City of San José provides mediation services to assist tenants and landlords with the negotiation process. Learn more about the <a href="http://www.sanjoseca.gov/DocumentCenter/View/57816">Rental Rights & Referral Program</a>.</p>
 
             <div className="list-group">
@@ -65,8 +86,6 @@ export default class FAQ extends React.Component {
                 <strong>Address: </strong>2310 North First Street, Suite 104, San José, CA 95131
               </p>
             </div>
-            <h4> What area does this app cover?</h4>
-            <p> Currently this application covers the City of San José. In the future, we hope to expand to other cities in Santa Clara County. </p>
           </div>
         </div>
       </RentersLayout>      
