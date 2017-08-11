27627339a6f3f10861c95540d63cbec2dc9a2b5d
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-08-10 17:29:25 -0700

    update message

--- client/app/bundles/RentersRights/components/NoDueToTPOMessage.jsx
+++ client/app/bundles/RentersRights/components/NoDueToTPOMessage.jsx
@@ -10,9 +10,8 @@ export default function NoDueToTPOMessage({
     >
       <div className="home-links">
         <div className="container">
-          <strong>
-            <h4>No, your landlord cannot legally evict you because under the Tenant Protection Ordinance, landlords must give one of 12 Just Cause Reasons for eviction. <a href="http://www.sanjoseca.gov/index.aspx?nid=5518">Learn More</a>.</h4>
-          </strong>
+          <h4>No, your landlord cannot legally evict you because under the Tenant Protection Ordinance, the landlord's notice must state one of 12 Just Cause reasons for termination. </h4>
+          <p><a href="http://www.sanjoseca.gov/index.aspx?nid=5518">Learn more about the Tenant Protection Ordinance</a>.</p>
           <p>
             <h4>Please confirm with a legal advisor and refer to our <a href="/resources">resource guide</a>.</h4>
           </p>
