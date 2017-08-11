e85bac65e70b5e2ff464f210c835eda080017b36
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-08-10 23:19:19 -0700

    updated routes for renamed paths

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'renters_rights#index'
   # reference: http://guides.rubyonrails.org/routing.html
   get '/faq', to: 'renters_rights#faq'
-  get '/no-due-to-tpo', to: 'renters_rights#no_due_to_tpo_message'
+  get '/tpo', to: 'renters_rights#tpo_message'
   get '/renters', to: 'renters_rights#renters'
   post '/renters', to: 'renters_rights#renters_post'
   get '/not-applicable', to: 'renters_rights#not_applicable'
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   post '/help-type', to: 'renters_rights#help_type_post'
   get '/renters-reasons', to: 'renters_rights#renters_reasons'
   post '/renters-reasons', to: 'renters_rights#renters_reasons_post'
-  get '/tenant-reallocation', to: 'renters_rights#tenant_reallocation'
+  get '/tenant-relocation', to: 'renters_rights#tenant_relocation'
   get '/eviction-resources', to: 'renters_rights#eviction_resources'
   get '/resources', to: 'renters_rights#resources'
 end
