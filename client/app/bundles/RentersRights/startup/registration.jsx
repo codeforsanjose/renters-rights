5ef79546b057c8850d39f83d664b9351724896ec
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-08-10 17:17:54 -0700

    register noduetoTPO and resources

--- client/app/bundles/RentersRights/startup/registration.jsx
+++ client/app/bundles/RentersRights/startup/registration.jsx
@@ -7,14 +7,18 @@ import ReactOnRails from 'react-on-rails';
 import Renters from '../components/Renters';
 import Home from '../components/Home';
 import FAQ from '../components/FAQ';
+import NoDueToTPOMessage from '../components/NoDueToTPOMessage';
+import Resources from '../components/Resources';
 
 // This is how react_on_rails can see the components in the browser.
 ReactOnRails.register({
   Home,
   FAQ,
+  Resources,
   EvictionResourcesMessage,
   HelpTypeForm,
   NotApplicableMessage,
+  NoDueToTPOMessage,
   Renters,
   RentersReasonsForm,
   TenantReallocationMessage,
