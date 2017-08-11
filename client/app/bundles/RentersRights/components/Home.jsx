f8c2ff962b70cd0b9c5e2b111ef16bf54d106497
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-08-10 22:52:12 -0700

    change layout homepage

--- client/app/bundles/RentersRights/components/Home.jsx
+++ client/app/bundles/RentersRights/components/Home.jsx
@@ -17,18 +17,13 @@ export default class Home extends React.Component {
           <div className="col-sm-12 col-md-7">
             <h4> How can we help you today? </h4>
             <div className="list-group">
-              <a href="/resources" className="list-group-item">
-                <h4 className="list-group-item-heading">I want to learn about housing programs.</h4>
-              </a>
-            </div>
-            <div className="list-group">
-              <a href="#" className="list-group-item">
-                <h4 className="list-group-item-heading">My landlord raised the rent and I cannot afford it anymore.</h4>
-              </a>
-            </div>
-            <div className="list-group">
               <a href="/renters" className="list-group-item">
                 <h4 className="list-group-item-heading">My landlord is trying to evict me and I don't know what to do.</h4>
+              </a>
+            </div>
+            <div className="list-group">
+              <a href="/resources" className="list-group-item">
+                <h4 className="list-group-item-heading">I want to learn about housing programs.</h4>
               </a>
             </div>
           </div>
