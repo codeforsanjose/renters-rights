9edd2dfb6850a3252a81b6fb9a392659ab11a6d9
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-08-10 23:19:31 -0700

    register renamed components

--- client/app/bundles/RentersRights/startup/registration.jsx
+++ client/app/bundles/RentersRights/startup/registration.jsx
@@ -1,5 +1,5 @@
 import RentersReasonsForm from '../components/RentersReasonsForm';
-import TenantReallocationMessage from '../components/TenantReallocationMessage';
+import TenantRelocationMessage from '../components/TenantRelocationMessage';
 import EvictionResourcesMessage from '../components/EvictionResourcesMessage';
 import HelpTypeForm from '../components/HelpTypeForm';
 import NotApplicableMessage from '../components/NotApplicableMessage';
@@ -7,7 +7,7 @@ import ReactOnRails from 'react-on-rails';
 import Renters from '../components/Renters';
 import Home from '../components/Home';
 import FAQ from '../components/FAQ';
-import NoDueToTPOMessage from '../components/NoDueToTPOMessage';
+import TPOMessage from '../components/TPOMessage';
 import Resources from '../components/Resources';
 
 // This is how react_on_rails can see the components in the browser.
@@ -18,8 +18,8 @@ ReactOnRails.register({
   EvictionResourcesMessage,
   HelpTypeForm,
   NotApplicableMessage,
-  NoDueToTPOMessage,
+  TPOMessage,
   Renters,
   RentersReasonsForm,
-  TenantReallocationMessage,
+  TenantRelocationMessage,
 });
