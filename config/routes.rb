8e90dc1439d52a4649ab28006a62611e8bcd9e39
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-08-10 17:18:49 -0700

    add routes for no and resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'renters_rights#index'
   # reference: http://guides.rubyonrails.org/routing.html
   get '/faq', to: 'renters_rights#faq'
+  get '/no-due-to-tpo', to: 'renters_rights#no_due_to_tpo_message'
   get '/renters', to: 'renters_rights#renters'
   post '/renters', to: 'renters_rights#renters_post'
   get '/not-applicable', to: 'renters_rights#not_applicable'
@@ -11,4 +12,5 @@ Rails.application.routes.draw do
   post '/renters-reasons', to: 'renters_rights#renters_reasons_post'
   get '/tenant-reallocation', to: 'renters_rights#tenant_reallocation'
   get '/eviction-resources', to: 'renters_rights#eviction_resources'
+  get '/resources', to: 'renters_rights#resources'
 end
