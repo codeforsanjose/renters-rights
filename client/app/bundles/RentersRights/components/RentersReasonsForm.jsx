0d3c9f91652d24a8e4b1ddaefac75a842e7ea31e
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-08-16 20:21:08 -0700

    move types to new form

--- client/app/bundles/RentersRights/components/RentersReasonsForm.jsx
+++ client/app/bundles/RentersRights/components/RentersReasonsForm.jsx
@@ -12,61 +12,6 @@ export default function RentersReasonsForm({
             className="form-horizontal"
             method="post"
           >
-            <h3>What type of home do you rent?</h3>
-            <div className="checkbox" required="true">
-              <label>
-                <input name="rentalType" type="radio" value=""/> Single Family Home
-              </label>
-              <br/>
-              <label>
-                <input name="rentalType" type="radio" value="duplex"/> Duplex 
-              </label>
-              <br/>
-              <label>
-                <input name="rentalType" type="radio" value="triplex"/> Triplex
-              </label>
-              <br/>
-              <label>
-                <input name="rentalType" type="radio" value="quadruplex"/> Quadruplex
-              </label>
-              <br/>
-              <label>
-                <input name="rentalType" type="radio" value="apt3Plut"/> Apartment with 3+ units in the building 
-              </label>
-              <br/>
-              <label>
-                input name="rentalType" type="radio" value="apt1To2"/> Apartment with 1-2 units in the building 
-              </label>
-              <br/>
-              <label>
-                <input name="rentalType" type="radio" value="mobileHome"/> Mobile Home 
-              </label>
-              <br/>
-            </div>
-              
-            <h3>When was your rental built? </h3>
-            <br/>
-            <label className="col-form-label" htmlFor="yearBuilt">Year Built:</label>
-              <input
-                className="form-control"
-                type="yearBuilt"
-              />
-  
-            <h3>Do you live in an unpermitted dwelling? </h3>
-            <em>Unpermitted dwelling means part  or parts of a structure that are being rented as a home, residence, or sleeping place, where the use is not
-authorized, permitted, or otherwise approved by the City</em>
-            <br/>
-            <div className="checkbox" required="true">
-              <label>
-                <input name="unpermitted" type="radio" value="yes"/> Yes
-              </label>
-              <br/>
-              <label>
-                <input name="unpermitted" type="radio" value="no"/> No
-              </label>
-              <br/>
-            </div>
-    
             <h3>Did your landlord give you reasons for evicting you in writing?</h3>
             <div className="checkbox" reyquired="true">
               <label>
