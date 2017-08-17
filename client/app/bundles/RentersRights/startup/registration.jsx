dab74b34b40bd28c65ff12efa0d0f32d262e3469
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-08-16 20:20:00 -0700

    register renters type form and remove old form

--- client/app/bundles/RentersRights/startup/registration.jsx
+++ client/app/bundles/RentersRights/startup/registration.jsx
@@ -1,7 +1,6 @@
 import RentersReasonsForm from '../components/RentersReasonsForm';
 import TenantRelocationMessage from '../components/TenantRelocationMessage';
 import EvictionResourcesMessage from '../components/EvictionResourcesMessage';
-import HelpTypeForm from '../components/HelpTypeForm';
 import NotApplicableMessage from '../components/NotApplicableMessage';
 import ReactOnRails from 'react-on-rails';
 import Renters from '../components/Renters';
@@ -9,14 +8,15 @@ import Home from '../components/Home';
 import FAQ from '../components/FAQ';
 import TPOMessage from '../components/TPOMessage';
 import Resources from '../components/Resources';
+import RentersType from '../components/RentersType';
 
 // This is how react_on_rails can see the components in the browser.
 ReactOnRails.register({
   Home,
   FAQ,
   Resources,
+  RentersType,
   EvictionResourcesMessage,
-  HelpTypeForm,
   NotApplicableMessage,
   TPOMessage,
   Renters,
