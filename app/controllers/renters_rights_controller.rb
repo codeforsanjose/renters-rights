227ade2fe297f8a7e16a9246f13909e025014eee
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-08-30 11:44:22 -0700

    remove unnecessary methods

--- app/controllers/renters_rights_controller.rb
+++ app/controllers/renters_rights_controller.rb
@@ -6,52 +6,14 @@ class RentersRightsController < ApplicationController
   def index
   end
 
-  def renters_post
-    if params[:city].strip.upcase == "SAN JOSE"
-      redirect_to '/renters-type'
-    else
-      redirect_to '/not-applicable'
-    end
+  def eviction
   end
 
-  def not_applicable
+  def renters
   end
-
-  def renters_type
-  end
-
-  def renters_type_post
-    if params[:apt3Plus] == "on" && params[:unpermitted] == "yes"
-      redirect_to '/renters-reasons'
-    else
-      redirect_to '/eviction-resources'
-    end
-  end
-
-  def renters_reasons
-  end
-
-  def renters_reasons_post
-    if params[:gaveReason] == "yes" && params[:ownerMoveIn] == "on" || params[:ellisAct] == "on" || params[:orderToVacate] == "on" || params[:leavingUnpermittedUnit] == "on" || params[:rehabilitation] == "on"
-      redirect_to '/tenant-relocation'
-    elsif params[:gaveReason] == "yes" && params[:nonpayment] == "on" ||  params[:lease] == "on" || params[:damage] == "on" || params[:refulsalNew] == "on" || params[:nuisance] == "on" || params[:refusing] == "on" || params[:sublease] == "on"
-      redirect_to '/eviction-resources'
-    elsif params[:gaveReason] == "no"
-      redirect_to '/tpo'
-    end 
-  end
-  
-  def tenant_relocation
-  end
-
-  def eviction_resources
-  end
-
-  def tpo_message
-  end 
 
   def resources
-  end 
+  end
 
   private
 
