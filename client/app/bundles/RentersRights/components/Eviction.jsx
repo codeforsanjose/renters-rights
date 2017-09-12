be8825546b8e420e968f769dd1a862af67fdbd43
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-09-11 20:34:19 -0700

    change layout

--- client/app/bundles/RentersRights/components/Eviction.jsx
+++ client/app/bundles/RentersRights/components/Eviction.jsx
@@ -36,7 +36,7 @@ export default class Eviction extends React.Component {
           'Tenant receives 3-day, 30-day, 60-day, or 90-day notice to move out.',
           'Tenant is served "Summons" and "Complaint - Unlawful Detainer".',
         ],
-        question: 'Does the tenant file a motion within 5 days?',
+        question: 'The tenant has five days to file a motion. Does the tenant file a motion within five days?',
         answers: [
           {
             title: 'Tenant files motion(s) within 5 days',
@@ -47,7 +47,7 @@ export default class Eviction extends React.Component {
                 'Landlord files "Request/Counter-Request to Set Case for Trial"',
                 'Notice of Hearing',
               ],
-              question: 'Is the trial held within 20 days?',
+              question: 'The trial is held within 20 days. At the trial, the judge or jury will decide in favor of tenant or landlord.',
               answers: [
                 {
                   title: 'Judgement for Tenant',
@@ -58,7 +58,7 @@ export default class Eviction extends React.Component {
                   },
                 },
                 {
-                  title: 'Judgement for Landlord',
+                  title: 'Judgment for Landlord',
                   nextStep: {
                     events: [
                       'Tenant receives Sheriff\'s 5-day eviction notice',
@@ -66,6 +66,7 @@ export default class Eviction extends React.Component {
                       'Sheriff evicts tenant(s)',
                     ],
                   },
+
                 },
               ],
             },
@@ -79,7 +80,7 @@ export default class Eviction extends React.Component {
                 'Optional: Tenant files "Motion to Set Aside Default and Default Judgement"',
                 'Tenant receives Sheriff\'s 5-day eviction notice',
                 'Optional: Tenant files "Motion for Stay of Execution" (to buy more days to stay)',
-                'Sheriff evicts tenant(s)',
+                'Sheriff evicts tenant(s)'
               ],
             },
           },
@@ -118,67 +119,155 @@ export default class Eviction extends React.Component {
       return (
         <RentersLayout>
 
-          <div className="content-container">
+          <div className="content-container language-paragraph">
             <div className="page-header">
               <h1>Tenant Eviction Process</h1>
             </div>
 
-            <p>
-              Select one of the buttons below to walk through the eviction process in the State of California.
-            </p>
-            <p> You can see the timeline of events on the right.
-            </p>
+            <p>This is a simulation of what may happen in the eviction process in the State of California. </p>
+            <p>If you are seeking legal aid, please consult a lawyer immediately. </p>
 
-            <div className="row">
-
-              <div className="col-md-6">
-                {currentStep.question && <form>
-                  <h4>{currentStep.question}</h4>
-                  {currentStep.answers.map((answer, i) => (
-                    <div
-                      className="form-group"
-                      key={i}
-                    >
-                      <button
-                        className="btn btn-default"
-                        onClick={() => this.handleNext(answer)}
-                        type="button"
-                      >
-                        {answer.title}
-                      </button>
-                    </div>
+            <div className="container">
+              <h3>Eviction Process Timeline in California</h3>
+              <table className="table">
+                <tbody>
+                  {events.map((event, i) => (
+                    <tr key={i}>
+                      <td>{i + 1}</td>
+                      <td>{event}</td>
+                    </tr>
                   ))}
-                </form>}
-                {!currentStep.question && <div>
-                  <p>
-                    See the Eviction Process Timeline on the right.
-                  </p>
-                  <button
-                    className="btn btn-primary"
-                    onClick={() => this.handleReset()}
-                    type="button"
+                </tbody>
+              </table>
+
+
+
+              {currentStep.question && <form>
+                <h4><em>{currentStep.question}</em></h4>
+                {currentStep.answers.map((answer, i) => (
+                  <div
+                    className="form-group"
+                    key={i}
                   >
-                    Reset
-                  </button>
+                    <button
+                      className="btn btn-default"
+                      onClick={() => this.handleNext(answer)}
+                      type="button"
+                    >
+                      {answer.title}
+                    </button>
+                  </div>
+                ))}
+              </form>}
+              {!currentStep.question && <div>
+                <p><em>You have reached the end of the simulation of the eviction process in California. To restart the simulation, click Reset.</em></p>
+                <button
+                  className="btn btn-primary"
+                  onClick={() => this.handleReset()}
+                  type="button"
+                >
+                  Reset
+                </button>
                 </div>}
-              </div>
-              <div className="col-md-6">
-                <h4>Timeline</h4>
-                <table className="table">
-                  <tbody>
-                    {events.map((event, i) => (
-                      <tr key={i}>
-                        <td>{i + 1}</td>
-                        <td>{event}</td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
             </div>
 
             <br/>
             <h4> Special Situations </h4>
+
+
+                <h3>Tenant Protection Ordinance (TPO)</h3>
+
+                <p>The City of San José Tenant Protection Ordinance (TPO) outlines landlord responsibilities and tenant rights regarding notices to terminate for properties covered by the ordinance. </p>
+                <br/>
+                <div className="panel-group" id="accordion">
+                  <div className="panel panel-info">
+                    <div className="panel-heading">
+                      <h4 className="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
+                        What is covered by TPO?</a>
+                      </h4>
+                    </div>
+                    <div id="collapse1" className="panel-collapse collapse">
+                      <div className="panel-body">
+                      <h5>Covered:</h5>
+                        <ul>
+                          <li>Rent stabilized apartments covered under ARO. Units within Apartments with 3+ units built on or before September 7, 1979</li>
+                          <li>Multifamily dwellings with at least 3 units</li>
+                          <li>Rental apartments except permitted hotels and motels</li>
+                          <li>Guest rooms in any guesthouse and guesthouses</li>
+                          <li>Units built without a permit</li>
+                        </ul>
+                      <h5>Not Covered:</h5>
+                        <ul>
+                          <li>Duplexes</li>
+                          <li>Single-family homes</li>
+                          <li>Permitted Second units</li>
+                          <li>Single family Condominiums</li>
+                          <li>Townhouses</li>
+                        </ul>
+                      <br/>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="panel panel-info">
+                    <div className="panel-heading">
+                      <h4 className="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">
+                        What does it mean to be covered under TPO?</a>
+                      </h4>
+                    </div>
+                    <div id="collapse2" className="panel-collapse collapse">
+                      <div className="panel-body">
+                        Buildings are covered by Just Cause and any 30-60-90 day notice to vacate must list one of the 12 just-cause reasons for eviction.
+                      </div>
+                    </div>
+                  </div>
+                  <div className="panel panel-info">
+                    <div className="panel-heading">
+                      <h4 className="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">
+                        What is Just Cause?</a>
+                      </h4>
+                    </div>
+                    <div id="collapse3" className="panel-collapse collapse">
+                      <div className="panel-body">
+                      <em> View this information in <a href="http://www.sanjoseca.gov/DocumentCenter/View/69112" target="_blank">Spanish</a> or <a href="http://www.sanjoseca.gov/DocumentCenter/View/69113" target="_blank">Vietnamese</a></em>
+                      <br/>
+                      <p><h5> Under TPO, landlords must include one of the following 12 reasons for termination in any 30-60-90 notice to vacate to the tenant:</h5></p>
+                      <li className="list-group-item active"><h4>Just Cause Terminations </h4> <p>The reasons for no-cause notice to vacate that signify fault on the part of the tenant are:</p></li>
+                      <li className="list-group-item">1. Nonpayment of rent</li>
+                      <li className="list-group-item">2. Material or habitual violation of the lease</li>
+                      <li className="list-group-item">3. Substantial damage to the apartment</li>
+                      <li className="list-group-item">4. Refusal to agree to a like or new rental agreement</li>
+                      <li className="list-group-item">5. Nuisance behavior</li>
+                      <li className="list-group-item">6. Refusing access to the apartment, requested in accordance in law</li>
+                      <li className="list-group-item">7. Unapproved holdover subtenant</li>
+                      <li className="list-group-item active"><h4>No-Fault Just Causes </h4><p>Relocation benefits must be paid when a Tenant is being removed from an apartment for reasons 8-12.</p></li>
+                      <li className="list-group-item">8. Substantial rehabilitation of the apartment</li>
+                      <li className="list-group-item">9. Ellis Act removal or demolition</li>
+                      <li className="list-group-item">10. Owner move-in</li>
+                      <li className="list-group-item">11. Order to vacate</li>
+                      <li className="list-group-item">12. Leaving an unpermitted apartment</li>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="panel panel-info">
+                    <div className="panel-heading">
+                      <h4 className="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#collapse4">
+                        No-cause Notice</a>
+                      </h4>
+                    </div>
+                    <div id="collapse4" className="panel-collapse collapse">
+                      <div className="panel-body">
+                      No-cause notices received after May 10, 2017 are no longer valid. If you have received this and are covered under TPO, please contact the City of San José Rental Rights & Referrals Program at (408)975-4480 to learn about your rights.
+                      </div>
+                    </div>
+                  </div>
+                  <br/>
+                  <p>To learn more, refer to the <a href="http://www.sanjoseca.gov/DocumentCenter/View/70042">Tenant Protection Ordinance Fact Sheet</a> and the <a href="http://www.sanjoseca.gov/DocumentCenter/View/70490">Tenant Protection Ordinance Frequently Asked Questions</a>.</p>
+                </div>
+
             <p>
               Learn more about <a href="http://www.courts.ca.gov/27953.htm" target="_blank">special situations</a>, or <a href="/renters">local ordinances in the City of San José</a>.
             </p>
@@ -191,13 +280,11 @@ export default class Eviction extends React.Component {
             </ul>
 
             <br/>
-            <p> To learn more about the Eviction Process in the State of California, visit the <a href="http://www.courts.ca.gov/27798.htm" target="_blank">California Courts</a> website.</p>
-
-
-              <a href="http://www.courts.ca.gov/partners/documents/eviction_process_flowchart.pdf">
-                <span className="glyphicon glyphicon-download-alt"></span> Download Eviction Process PDF
+            <p> To learn more about the Eviction Process in the State of California, visit the <a href="http://www.courts.ca.gov/27798.htm" target="_blank">California Courts</a> website or download
+              <a href="http://www.courts.ca.gov/partners/documents/eviction_process_flowchart.pdf" target="_blank">
+                <span className="glyphicon glyphicon-download-alt"></span> the Eviction Process PDF.
               </a>
-
+            </p>
           </div>
         </RentersLayout>
       );
