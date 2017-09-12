9dec0c406940c15cb7d4393d96faa3280b826c0c
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-09-11 17:46:05 -0700

    update links

--- client/app/bundles/RentersRights/components/RentersFooter.jsx
+++ client/app/bundles/RentersRights/components/RentersFooter.jsx
@@ -9,16 +9,8 @@ export default class RentersFooter extends React.Component {
         <br/>
         <br/>
         <div className="container">
-          <ul className="nav nav-pils nav-justified">
-            <li className="nav-item">
-              <a className="nav-link" href="resources">Resources</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="FAQ">FAQ</a>
-            </li>
-          </ul>
-
-          <p className="text-center"> <em>Disclaimer:</em> This app informs you about your basic rights and resources. It is not a substitute for legal advice. Find a <a href="https://lawhelpca.org/issues/housing/landlord-and-tenant-issues">legal aid office</a>.</p>
+          <p className="text-center"> <em>Disclaimer:</em> This app only covers San José, California. This app is not a substitute for legal advice. If you are seeking legal aid, please consult a lawyer immediately. Find a <a href="https://lawhelpca.org/issues/housing/landlord-and-tenant-issues">legal aid office</a>.</p>
+          <p className="text-center">© 2017 Code for San José</p>
         </div>
       </footer>
     )
