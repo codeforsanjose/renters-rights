4b2f8835e970e79fd41c473bcb3dcf494440e356
Matthew King <mking@users.noreply.github.com>
2017-10-17 21:58:24 -0700

    add intl example

--- client/app/bundles/RentersRights/components/RentersFooter.jsx
+++ client/app/bundles/RentersRights/components/RentersFooter.jsx
@@ -1,3 +1,4 @@
+import { FormattedMessage } from 'react-intl';
 import React from 'react';
 
 export default class RentersFooter extends React.Component {
@@ -9,7 +10,31 @@ export default class RentersFooter extends React.Component {
         <br/>
         <br/>
         <div className="content-container">
-          <p className="text-center"> <em>Disclaimer:</em> This app only covers San José, California. This app is not a substitute for legal advice. If you are seeking legal aid, please consult a lawyer immediately. Find a <a href="https://lawhelpca.org/issues/housing/landlord-and-tenant-issues">legal aid office</a>.</p>
+          <p className="text-center">
+            <FormattedMessage
+              defaultMessage="{label} This app only covers San José, California. This app is not a substitute for legal advice. If you are seeking legal aid, please consult a lawyer immediately. Find a {link}."
+              id="footer.disclaimer"
+              values={{
+                label: (
+                  <em>
+                    <FormattedMessage
+                      defaultMessage="Disclaimer"
+                      id="footer.label"
+                    />
+                    :
+                  </em>
+                ),
+                link: (
+                  <a href="https://lawhelpca.org/issues/housing/landlord-and-tenant-issues">
+                    <FormattedMessage
+                      defaultMessage="legal aid office"
+                      id="footer.link"
+                    />
+                  </a>
+                ),
+              }}
+            />
+          </p>
           <p className="text-center">© 2017 Code for San José</p>
         </div>
       </footer>
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root 'renters_rights#index'
+  root to: redirect('/en')
   # reference: http://guides.rubyonrails.org/routing.html
-  get '/faq', to: 'renters_rights#faq'
-  get '/renters', to: 'renters_rights#renters'
-  get '/eviction', to: 'renters_rights#eviction'
-  get '/resources', to: 'renters_rights#resources'
+  get '/:locale', to: 'renters_rights#index'
+  get '/:locale/faq', to: 'renters_rights#faq'
+  get '/:locale/renters', to: 'renters_rights#renters'
+  get '/:locale/eviction', to: 'renters_rights#eviction'
+  get '/:locale/resources', to: 'renters_rights#resources'
 end
