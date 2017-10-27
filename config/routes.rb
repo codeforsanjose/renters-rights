edb5da6854ecfe7da2d5fabc5c855e6ee84aef3a
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-10-27 16:59:28 -0700

    move rent-calculator to bottom

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
   root to: redirect('/en')
   # reference: http://guides.rubyonrails.org/routing.html
-  get '/faq', to: 'renters_rights#faq'
-  get '/renters', to: 'renters_rights#renters'
-  get '/eviction', to: 'renters_rights#eviction'
-  get '/resources', to: 'renters_rights#resources'
-  get '/rent-calculator', to: 'renters_rights#rent_calculator'
+  # get '/faq', to: 'renters_rights#faq'
+  # get '/renters', to: 'renters_rights#renters'
+  # get '/eviction', to: 'renters_rights#eviction'
+  # get '/resources', to: 'renters_rights#resources'
+  # get '/rent-calculator', to: 'renters_rights#rent_calculator'
 
-#  get '/rent-calculator', to: 'renters_rights#rent_calculator'
-#  get '/:locale', to: 'renters_rights#index'
-#  get '/:locale/faq', to: 'renters_rights#faq'
-#  get '/:locale/renters', to: 'renters_rights#renters'
-#  get '/:locale/eviction', to: 'renters_rights#eviction'
-#  get '/:locale/resources', to: 'renters_rights#resources'
-
+  get '/:locale', to: 'renters_rights#index'
+  get '/:locale/faq', to: 'renters_rights#faq'
+  get '/:locale/renters', to: 'renters_rights#renters'
+  get '/:locale/eviction', to: 'renters_rights#eviction'
+  get '/:locale/resources', to: 'renters_rights#resources'
+  get '/:locale/rent-calculator', to: 'renters_rights#rent_calculator'
 end
