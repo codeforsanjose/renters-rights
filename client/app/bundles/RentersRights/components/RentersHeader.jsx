6c7e2953b5c7dd8056d5cf025d540da88070540b
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-10-27 17:08:50 -0700

    hide dropdown

--- client/app/bundles/RentersRights/components/RentersHeader.jsx
+++ client/app/bundles/RentersRights/components/RentersHeader.jsx
@@ -29,6 +29,7 @@ export default function RentersHeader({ className, locale }) {
 						<li><a href="/faq"><strong>FAQ</strong></a></li> */} 
             <li><a href={`/${locale}/resources`}><strong><FormattedMessage defaultMessage="Resources" id="header.resources" /></strong></a></li>
 						<li><a href={`/${locale}/faq`}><strong><FormattedMessage defaultMessage="FAQ" id="header.faq" /></strong></a></li>
+        {/* 
 						<li className="dropdown">
 							<a 
 								className="dropdown-toggle"
@@ -50,6 +51,7 @@ export default function RentersHeader({ className, locale }) {
 								))}
 							</ul>
 						</li>
+        */}
 					</ul>
 					</div>
 				</div>
