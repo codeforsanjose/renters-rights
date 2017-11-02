0ea6bff785abc5e3d3c27bfbc9ca0922ec03e30b
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-11-01 18:11:19 -0700

    comment out locale set up

--- client/app/bundles/RentersRights/startup/registration.jsx
+++ client/app/bundles/RentersRights/startup/registration.jsx
@@ -5,10 +5,10 @@ import Eviction from '../components/Eviction';
 import FAQ from '../components/FAQ';
 import Resources from '../components/Resources';
 import RentCalculator from '../components/RentCalculator';
-import { addLocaleData } from 'react-intl';
-import esLocaleData from 'react-intl/locale-data/es';
+//import { addLocaleData } from 'react-intl';
+//import esLocaleData from 'react-intl/locale-data/es';
 
-addLocaleData(esLocaleData);
+//addLocaleData(esLocaleData);
 
 // This is how react_on_rails can see the components in the browser.
 ReactOnRails.register({
