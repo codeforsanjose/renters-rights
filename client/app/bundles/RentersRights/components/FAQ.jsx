fb7ae7025be1f74e99cb0690ad76ac276b7cbec6
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-11-13 13:01:08 -0800

    change class to className

--- client/app/bundles/RentersRights/components/FAQ.jsx
+++ client/app/bundles/RentersRights/components/FAQ.jsx
@@ -20,17 +20,17 @@ export default class FAQ extends React.Component {
             </h1>
           </div>
 
-          <div class="panel-group" id="accordion">
-            <div class="panel panel-default">
-              <div class="panel-heading">
-                <h3 class="panel-title">
+          <div className="panel-group" id="accordion">
+            <div className="panel panel-default">
+              <div className="panel-heading">
+                <h3 className="panel-title">
                   <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
                   Who is behind this app?
                   </a>
                 </h3>
               </div>
-              <div id="collapse1" class="panel-collapse collapse in">
-                <div class="panel-body">
+              <div id="collapse1" className="panel-collapse collapse in">
+                <div className="panel-body">
                 We are a group of volunteer developers, desginers and researchers concerned about the housing crisis in Silicon Valley. We are involved with the <a href="http://codeforsanjose.com" target="_blank">San José chapter</a> of <a href="https://www.codeforamerica.org/" target="_blank">Code for America</a>.
                 <p>Team:</p>
                 <ul>
@@ -45,28 +45,28 @@ export default class FAQ extends React.Component {
               </div>
             </div>
 
-            <div class="panel panel-default">
-              <div class="panel-heading">
-                <h3 class="panel-title">
+            <div className="panel panel-default">
+              <div className="panel-heading">
+                <h3 className="panel-title">
                   <a data-toggle="collapse" data-parent="#accordion" href="#collapse4">
                   Who can I contact about an eviction?</a>
                 </h3>
               </div>
-              <div id="collapse4" class="panel-collapse collapse">
-                <div class="panel-body">
+              <div id="collapse4" className="panel-collapse collapse">
+                <div className="panel-body">
                 Please visit our <a href="/eviction">tenant eviction page</a> to learn more or check out our <a href="/resources">resources</a>.
                 </div>
               </div>
             </div>
-            <div class="panel panel-default">
-              <div class="panel-heading">
-                <h3 class="panel-title">
+            <div className="panel panel-default">
+              <div className="panel-heading">
+                <h3 className="panel-title">
                   <a data-toggle="collapse" data-parent="#accordion" href="#collapse5">
                   Where can I go for help if I live outside of San Jose?</a>
                 </h3>
               </div>
-              <div id="collapse5" class="panel-collapse collapse">
-                <div class="panel-body">
+              <div id="collapse5" className="panel-collapse collapse">
+                <div className="panel-body">
                 Please refer to our list of <a href="/resources">resources</a>.
                 </div>
               </div>
