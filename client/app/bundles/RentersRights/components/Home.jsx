7cc3388a6e6590c68c27d3882d5ba66c02917ddc
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-11-13 15:02:26 -0800

    fix merge conflicts

--- client/app/bundles/RentersRights/components/Home.jsx
+++ client/app/bundles/RentersRights/components/Home.jsx
@@ -21,7 +21,7 @@ export default class Home extends React.Component {
               />
             </h3>
           </div>
-          <br/>
+          <br//>
           <div className="home-links">
             <div className="col-sm-12 col-md-6">
               <h3>
@@ -77,27 +77,30 @@ export default class Home extends React.Component {
            <div className="page-header">
              <h3>Helping renters in San José, California understand the laws and programs in place to protect them.
              </h3>
-            </div>
-            <br/>
-            <div className="home-links">
-             <div className="col-sm-12 col-md-6">
-                <h3> How can we help you today? </h3>
-                <div className="list-group">
-                  <a href="/eviction" className="list-group-item list-group-item-danger">
-                   <h4 className="list-group-item-heading">I want to learn about tenant eviction.</h4>
-                 </a>
-               </div>
-               <div className="list-group">
-                 <a href="/renters" className="list-group-item list-group-item-danger">
-                   <h4 className="list-group-item-heading">I want to learn about renters rights.</h4>
-                 </a>
-               </div>
-                <div className="list-group">
-                 <a href="/resources" className="list-group-item list-group-item-danger">
-                   <h4 className="list-group-item-heading">I want to learn about local resources.</h4>
-                 </a>
-               </div>
-             </div>
+           </div>
+           <div className="row">
+              <div className="col-sm-6 col-md-12">
+                <p>
+                  <a href="/eviction" className="btn btn-sq-lg btn-primary">
+                    <span className="glyphicon glyphicon-file"></span> <br/>I Am Getting Evicted
+                  </a>
+                  <a href="/renters" className="btn btn-sq-lg btn-success">
+                    <span className="glyphicon glyphicon-signal"></span> <br/>My Rent Is Increasing
+                  </a>
+                  <a href="/#" className="btn btn-sq-lg btn-warning">
+                   <span className="glyphicon glyphicon-home"></span> <br/>  I Need Shelter
+                  </a>
+                  <a href="#" className="btn btn-sq-lg btn-info">
+                    <span className="glyphicon glyphicon-pencil"></span> <br/>I Want To Report An Issue
+                  </a>
+                  <a href="/renters" className="btn btn-sq-lg btn-warning">
+                    <span className="glyphicon glyphicon-map-marker"></span> <br/>Do I Live Within San Jose?
+                  </a>
+                  <a href="#" className="btn btn-sq-lg btn-primary">
+                    <span className="glyphicon glyphicon-question-sign"></span> <br/>I Need Legal Help
+                  </a>
+                </p>
+              </div>
            </div>
          </div>
        </RentersLayout>
