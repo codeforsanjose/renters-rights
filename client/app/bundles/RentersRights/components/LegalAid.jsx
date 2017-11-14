540b69afa22a5fd6aa72f5a9fb6f9c323e5338ed
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-11-13 17:20:34 -0800

    add Legal Aid component

---
+++ client/app/bundles/RentersRights/components/LegalAid.jsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import RentersLayout from './RentersLayout'
+
+export default class LegalAid extends React.Component {
+  render() {
+   {/* locale setup - remove comment when app translation is ready. Also add locale={locale} to RentersLayout below
+       const { locale } = this.props; */} 
+    const {} = this.props;
+    return (
+      <RentersLayout>
+                  <br/><h5>Tenants Together</h5>
+                  <p>(888) 495 8020</p>
+                  <p>Provides tenant counseling over the phone to tenants across the state certain days of the week</p>
+
+                  <br/><h5>Law Foundation of Silicon Valley - Volunteer Eviction Assistance Clinic</h5>
+                  <p>(408) 280-2424</p>
+                  <p>Provides limited assistance to Santa Clara County tenants facing eviction</p>
+
+                  <br/><h5>Law Foundation of Silicon Valley - Fair Housing Law Project</h5>
+                  <p>(408) 280-2452</p>
+                  <p>Provides free legal representation to victims of housing discrimination</p>
+
+                  <br/><h5>Law Foundation of Silicon Valley - Mental Health Advocacy Project</h5>
+                  <p>(408) 280-2420</p>
+                  <p>Provides free legal representation to mental health and developmental services consumers who have housing problems</p>
+
+                  <br/><h5>Law Foundation of Silicon Valley - Health Legal Services</h5>
+                  <p>(408) 280-2430</p>
+                  <p>Provides free legal representation to individuals with HIV/AIDS or diabetes who have housing problems</p>
+
+                  <br/><h5>Project Sentinel</h5>
+                  <p>1-888-331-3332</p>
+                  <p>Provides a variety of services (including mediation services) and advice regarding landlord-tenant problems in some jurisdictions.  Investigates claims of housing discrimination.</p>
+
+                  <br/><h5>Bay Area Legal Aid</h5>
+                  <p>(408) 850-7066</p>
+                  <p>Provides free legal representation to low-income tenants with housing problems</p>
+
+                  <br/><h5>Asian Law Alliance</h5>
+                  <p>(408) 287-9710</p>
+                  <p>Provides free legal representation to tenants with housing problems</p>
+
+                  <br/><h5>Pro Bono Project Lawyers in the Library</h5>
+                  <p>(408) 808-2382</p>
+                  <p>Provides free 20-minute legal consultation on a variety of legal matters</p>
+
+                  <br/>
+                  <h4>Santa Clara County</h4>
+                  <h5>SCC Department of Human Relations, Dispute Resolution Services</h5>
+                  <p>(408) 792-2314</p>
+                  <p>Provides low cost neutral-third-party dispute resolution services between tenants and landlords, tenants and other tenants, and neighbors.</p>
+
+                  <br/><h5>California Department of Fair Employment and Housing</h5>
+                  <p>1-800-233-3212	</p>
+                  <p>State agency responsible for taking complaints of housing discrimination</p>
+
+                  <br/><h5>City of San José - Rental Rights & Referral Program</h5>
+                  <p>(408)975-4480</p>
+                  <p>Rent increases and service reductions in mobilehomes and most tri-plex and larger buildings built before 9/7/79</p>
+
+                  <br/><h5>City of San José - Code Enforcement</h5>
+                  <p>(408)535-7770</p>
+                  <p>Enforces Housing Code and other City code</p>
+
+                  <br/><h5>Senior Adult Legal Assistance</h5>
+                  <p>(408)295-5991</p>
+                  <p>Legal assistance for individuals 60+ years old</p>
+
+                  <br/><h5>Santa Clara County Bar Association Lawyer Referral</h5>
+                  <p>(408)287-2557</p>
+                  <p>Referrals to private attorneys (commercial rent disputes, other legal problems)</p>
+
+                  <br/><h5>Volunteer Eviction Assistance</h5>
+                  <p>(408) 280-2424</p>
+                  <p>Short-term pro-bono assistance with eviction complaints Fridays by appointment.</p>
+
+                  <br/><h5>Palo Alto Mediation Service</h5>
+                  <p>(650) 856-4062</p>
+                  <p>Information, counseling and mediation</p>
+
+                  <br/><h5>Los Gatos Information and Referral Services</h5>
+                  <p>(408)243-8565</p>
+                  <p>Information, counseling and mediation Apartment and mobilehome rent control</p>
+
+                  <br/><h5>Los Altos Mediation Program</h5>
+                  <p>(650) 949-5267</p>
+                  <p>Information and mediation</p>
+
+                  <br/>
+                  <h4> San Mateo County </h4>
+                  <h5>Community Legal Services East Palo Alto</h5>
+                  <p>650-326-6440</p>
+                  <p>Provides free legal aid to tenants</p>
+      </RentersLayout>
+    )
+  }
+}
