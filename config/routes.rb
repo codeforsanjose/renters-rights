b14ab73c12a968b1294f26a876db814b2733e2ee
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-02-06 01:52:19 -0800

    config route to Rent Increase page

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get '/shelter', to: 'renters_rights#shelter'
   get '/report-issue', to: 'renters_rights#report_issue'
   get '/find-jurisdiction', to: 'renters_rights#find_jurisdiction'
+  get '/rent-increase', to: 'renters_rights#rent_increase'
   # get '/:locale', to: 'renters_rights#index'
   # get '/:locale/about', to: 'renters_rights#faq'
   # get '/:locale/renters', to: 'renters_rights#renters'
