0768bd1ee14f9ebfee9a5459b507ccb3a5db9b3d
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-02-06 02:48:09 -0800

    replace Renters with Eviction route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root to: 'renters_rights#index'
   # reference: http://guides.rubyonrails.org/routing.html
   get '/about', to: 'renters_rights#about'
-  get '/renters', to: 'renters_rights#renters'
   get '/eviction', to: 'renters_rights#eviction'
   get '/resources', to: 'renters_rights#resources'
   get '/rent-calculator', to: 'renters_rights#rent_calculator'
