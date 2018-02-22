82fd4752675f37c034162894c901884cfb4b15cc
chap <chaplonglau@gmail.com>
2018-02-21 17:49:50 -0800

    error page

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,23 @@ module RentersRightsApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.exception_handler = { 
+      dev: true,
+      layouts: {
+        404 => "errors",
+        422 => "errors",
+        500 => "errors",
+        501 => "errors",
+        502 => "errors",
+        503 => "errors",
+        504 => "errors",
+        505 => "errors",
+        507 => "errors",
+        510 => "errors"
+      }
+    }
 
+    
     I18n.enforce_available_locales = false    
   end
 end
