6b7a367393b3ee3b4201de93a282ec0c86728ad0
Guy Wassather <ggwass@gmail.com>
2018-03-22 20:36:25 -0700

    Add class names to use bootstrap styling

--- client/app/bundles/RentersRights/components/ResourceIndexItem.jsx
+++ client/app/bundles/RentersRights/components/ResourceIndexItem.jsx
@@ -17,13 +17,14 @@ export default class ResourceIndexItem extends React.Component {
     } = this.props.resource;
 
     return (
-      <div>
-        <a href={website}><h3>{organization}</h3></a>
-        <p>{description}</p>
-        <span>Contact: </span>
-        <p>{email}</p>
-        <p>{phone}</p>
-        <p>{address}</p>
+      <div className="row">
+        <div className="col-md-9">
+          <a href={website}><h3>{organization}</h3></a>
+          <p>{description}</p>
+          <span>Contact: </span>
+          <p>Phone: {phone}</p>
+          <p>Address: {address}</p>
+        </div>
       </div>
     )
   }
