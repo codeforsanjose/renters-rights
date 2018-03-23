8a4397183ef1a940007749ef180bb8e6d6434a1d
Guy Wassather <ggwass@gmail.com>
2018-03-23 08:57:31 -0700

    Adjust styling to match what it looked like before

--- client/app/bundles/RentersRights/components/ResourceIndexItem.jsx
+++ client/app/bundles/RentersRights/components/ResourceIndexItem.jsx
@@ -17,14 +17,12 @@ export default class ResourceIndexItem extends React.Component {
     } = this.props.resource;
 
     return (
-      <div className="row">
-        <div className="col-md-9">
-          <a href={website}><h3>{organization}</h3></a>
-          <p>{description}</p>
-          <p>Contact: </p>
-          <p><span className="glyphicon glyphicon-earphone"></span>   Phone: {phone}</p>
-          <p><span className="glyphicon glyphicon-home"></span>  Address: {address}</p>
-        </div>
+      <div>
+        <a href={website}><h3>{organization}</h3></a>
+        <p>{description}</p>
+        <p>Contact: </p>
+        <p><span className="glyphicon glyphicon-earphone"></span>   Phone: {phone}</p>
+        <p><span className="glyphicon glyphicon-home"></span>  Address: {address}</p>
       </div>
     )
   }
--- client/app/bundles/RentersRights/components/Resources.jsx
+++ client/app/bundles/RentersRights/components/Resources.jsx
@@ -14,14 +14,25 @@ export default class Resources extends React.Component {
     const {} = this.props;
     return (
       <RentersLayout>
-        {
-          resourceList.map((resource, idx) => (
-            <ResourceIndexItem
-              resource={resource}
-              key={idx}
-              />
-          ))
-        }
+        <div className='content-container'>
+          <div className="row">
+            <div className="col-md-9">
+              {
+                resourceList.map((resource, idx) => (
+                  <ResourceIndexItem
+                    resource={resource}
+                    key={idx}
+                    />
+                ))
+              }
+            </div>
+            <div className="col-md-3">
+              <p>For Immediate Assistance:</p>
+              <p>Call Emergency Bed Hotline at 1(800)7-SHELTER (1.800.7.743.5837) (Spanish/English)</p>
+              <p> More questions? Check out <a href="https://www.1degree.org" target="_blank">One Degree</a> for more resources in the San Francisco Bay Area. </p>
+            </div>
+          </div>
+        </div>
       </RentersLayout>
     )
   }
