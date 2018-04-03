1c27949030b361f6d4ff3e1dd289d66d8f858676
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-04-02 22:43:35 -0700

    fix form - set event target name to value to retrieve values from multiple inputs

--- client/app/bundles/RentersRights/components/AddressCheck.jsx
+++ client/app/bundles/RentersRights/components/AddressCheck.jsx
@@ -3,6 +3,10 @@ import RentersLayout from './RentersLayout'
 
 export default class AddressCheck extends React.Component {
 
+  /*
+    Set fields on this component's state to handle
+    the state of the input fields
+   */
    constructor(props) {
      super(props);
      this.state = {
@@ -12,36 +16,42 @@ export default class AddressCheck extends React.Component {
        city: '',
        zip: '',
      }
-
+    /*
+      Collect values in the input fields using onChange listeners
+     */
      this.handleChange = this.handleChange.bind(this);
      this.handleSubmit = this.handleSubmit.bind(this);
    }
 
+
    handleChange(event) {
+     /*
+        Get the event target name (e.g. "street") and use it to target
+        the key on the state object with the same name, using bracket syntax
+      */
      this.setState({
-       street: event.target.street,
-       aptOrUnit: event.target.aptOrUnit,
-       state: event.target.state,
-       city: event.target.city,
-       zip: event.target.zip,
+       [event.target.name]: event.target.value
      });
+
    }
 
    handleSubmit(event) {
-     console.log(this.state.street);
-     console.log(this.state.city);
-     alert('A street was submitted: ' + this.state.street );
+     // console.log(this.state.street);
+     // console.log(this.state.aptOrUnit);
+     // console.log(this.state.city);
+     // console.log(this.state.state);
+     // console.log(this.state.zip);
      event.preventDefault();
    }
 
 
-  addressCheck(key,address,citystatezip) {
-
-    key="X1-ZWz18q9c4vs1e3_af1tq";
-    url="http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=#{key}&address=#{address}&citystatezip=#{citystatezip}";
-    $.getJSON(url);
-
-  }
+  // addressCheck(key,address,citystatezip) {
+  //
+  //   key="X1-ZWz18q9c4vs1e3_af1tq";
+  //   url="http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=#{key}&address=#{address}&citystatezip=#{citystatezip}";
+  //   $.getJSON(url);
+  //
+  // }
 
   /*
   componentDidMount() {
@@ -55,86 +65,75 @@ export default class AddressCheck extends React.Component {
 
   */
   render(){
-
-
     return (
       <RentersLayout>
         <div className="content-container">
           <div className="page-header">
-             <h3>
-              Address Check
-             </h3>
            </div>
 
-          <div className="container">
-            <div className="row">
-              <form onSubmit={this.handleSubmit}>
-                <h3> What is your address?</h3>
-                <div className="form-group row">
-                  <label className="col-md-1 col-form-label" htmlFor="street">Street Address:</label>
-                  <div className="col-md-8">
-                    <input
-                      className="form-control"
-                      value={this.state.street}
-                      name="street"
-                      type="text"
-                      onChange={this.handleChange}
-                    />
-                  </div>
-                  <label className="col-md-1 col-form-label" htmlFor="aptOrUnit">Apt/Unit:</label>
-                  <div className="col-md-2">
-                    <input
-                      className="form-control"
-                      value={this.state.aptOrUnit}
-                      name="aptOrUnit"
-                      type="text"
-                      onChange={this.handleChange}
-                    />
-                  </div>
+          <div className="row">
+            <form onSubmit={this.handleSubmit}>
+              <h3> What is your address?</h3>
+              <div className="form-group row">
+                <label className="col-sm-1 col-md-1 col-form-label" htmlFor="street">Street Address:</label>
+                <div className="col-sm-8 col-md-8">
+                  <input
+                    className="form-control"
+                    name="street"
+                    type="text"
+                    onChange={this.handleChange}
+                  />
                 </div>
-                <div className="form-group row">
-                  <label className="col-md-1 col-form-label" htmlFor="city">City:</label>
-                  <div className="col-md-4">
-                    <input
-                      className="form-control"
-                      value={this.state.city}
-                      name="city"
-                      type="text"
-                      onChange={this.handleChange}
-                    />
-                  </div>
-                  <label className="col-md-1 col-form-label" htmlFor="state">State:</label>
-                  <div className="col-md-2">
-                    <input
-                      className="form-control"
-                      value={this.state.state}
-                      name="state"
-                      type="text"
-                      onChange={this.handleChange}
-                    />
-                  </div>
-                  <label className="col-md-1 col-form-label" htmlFor="zip">Zip:</label>
-                  <div className="col-md-3">
-                    <input
-                      className="form-control"
-                      value={this.state.zip}
-                      name="city"
-                      type="text"
-                      onChange={this.handleChange}
-                    />
-                  </div>
+                <label className="col-sm-1 col-md-1 col-form-label" htmlFor="aptOrUnit">Apt/Unit:</label>
+                <div className="col-sm-2 col-md-2">
+                  <input
+                    className="form-control"
+                    name="aptOrUnit"
+                    type="text"
+                    onChange={this.handleChange}
+                  />
                 </div>
-                <button
-                  className="btn btn-primary"
-                  type="submit"
-                  value="Submit"
-                >
-                  Get Started
-                </button>
-              </form>
-            </div>
+              </div>
+              <div className="form-group row">
+                <label className="col-sm-1 col-md-1 col-form-label" htmlFor="city">City:</label>
+                <div className="col-sm-4 col-md-4">
+                  <input
+                    className="form-control"
+                    name="city"
+                    type="text"
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <label className="col-sm-1 col-md-1 col-form-label" htmlFor="state">State:</label>
+                <div className="col-sm-2 col-md-2">
+                  <input
+                    className="form-control"
+                    name="state"
+                    type="text"
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <label className="col-sm-1 col-md-1 col-form-label" htmlFor="zip">Zip:</label>
+                <div className="col-sm-3 col-md-3">
+                  <input
+                    className="form-control"
+                    name="city"
+                    type="text"
+                    onChange={this.handleChange}
+                  />
+                </div>
+              </div>
+              <button
+                className="btn btn-primary"
+                type="submit"
+                value="Submit"
+              >
+                Get Started
+              </button>
+            </form>
           </div>
-       </div>
+        </div>
+
      </RentersLayout>
     );
   }
