a83123d067ff17ebb67913e78761a7ba887fd0e1
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-04-03 08:29:15 -0700

    add form helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,8 @@ module ApplicationHelper
 #  def read_only_component(name)
 #    react_component(name, props: { locale: I18n.locale })
 #  end
-  
-#  def form_component(name)
-#    react_component(name, props: { token: form_authenticity_token, locale: I18n.locale })
-#  end
+
+ def form_component(name)
+   react_component(name, props: { token: form_authenticity_token})
+ end
 end
