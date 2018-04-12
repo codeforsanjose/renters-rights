9b422df6c064ef1f2b50b5c8811b9c1fac19a0ca
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-04-11 22:24:52 -0700

    update conditional

--- app/helpers/renters_rights_helper.rb
+++ app/helpers/renters_rights_helper.rb
@@ -4,16 +4,17 @@ module RentersRightsHelper
     url="http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=#{key}&address=#{address}&citystatezip=#{citystatezip}"
     puts url
     page=HTTParty.get(url)
+    binding.pry
     if (page.parsed_response["searchresults"]["message"]["code"] == "0")
       # if it only returns one address, do this 
-      if page.parsed_response["searchresults"]["response"]["results"]
+      if page.parsed_response["searchresults"]["response"]["results"]["result"]["localRealEstate"]["region"]["type"]
         type=page.parsed_response["searchresults"]["response"]["results"]["result"]["localRealEstate"]["region"]["type"]
         useCode=page.parsed_response["searchresults"]["response"]["results"]["result"]["useCode"]
         yearBuilt=page.parsed_response["searchresults"]["response"]["results"]["result"]["yearBuilt"]
         street=page.parsed_response["searchresults"]["response"]["results"]["result"]["address"]["street"]
         return [type,useCode,yearBuilt,street]
       # if it returns multiple addresses 
-      elsif page.parsed_response["searchresults"]["response"]["results"]["result"][0]
+      elsif page.parsed_response["searchresults"]["response"]["results"]["result"][0]["localRealEstate"]["region"]["type"]
         type=page.parsed_response["searchresults"]["response"]["results"]["result"][0]["localRealEstate"]["region"]["type"]
         useCode=page.parsed_response["searchresults"]["response"]["results"]["result"][0]["useCode"]
         yearBuilt=page.parsed_response["searchresults"]["response"]["results"]["result"][0]["yearBuilt"]
