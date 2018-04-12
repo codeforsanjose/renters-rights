6eaef705a4bceeae85fcc21637bef3222c6ad436
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-04-11 22:01:51 -0700

    fix conditional so that it renders data if XML response has multiple elements

--- app/helpers/renters_rights_helper.rb
+++ app/helpers/renters_rights_helper.rb
@@ -5,19 +5,21 @@ module RentersRightsHelper
     puts url
     page=HTTParty.get(url)
     if (page.parsed_response["searchresults"]["message"]["code"] == "0")
-          binding.pry
-
-      # if it only returns one address, this works  
-
+      # if it only returns one address, do this 
       if page.parsed_response["searchresults"]["response"]["results"]
-      type=page.parsed_response["searchresults"]["response"]["results"]["result"]["localRealEstate"]["region"]["type"]
-      #
-      page.parsed_response["searchresults"]["response"]["results"]["result"][0]["localRealEstate"]["region"]["type"]
-
-      useCode=page.parsed_response["searchresults"]["response"]["results"]["result"]["useCode"]
-      yearBuilt=page.parsed_response["searchresults"]["response"]["results"]["result"]["yearBuilt"]
-      street=page.parsed_response["searchresults"]["response"]["results"]["result"]["address"]["street"]
-      return [type,useCode,yearBuilt,street]
+        type=page.parsed_response["searchresults"]["response"]["results"]["result"]["localRealEstate"]["region"]["type"]
+        useCode=page.parsed_response["searchresults"]["response"]["results"]["result"]["useCode"]
+        yearBuilt=page.parsed_response["searchresults"]["response"]["results"]["result"]["yearBuilt"]
+        street=page.parsed_response["searchresults"]["response"]["results"]["result"]["address"]["street"]
+        return [type,useCode,yearBuilt,street]
+      # if it returns multiple addresses 
+      elsif page.parsed_response["searchresults"]["response"]["results"]["result"][0]
+        type=page.parsed_response["searchresults"]["response"]["results"]["result"][0]["localRealEstate"]["region"]["type"]
+        useCode=page.parsed_response["searchresults"]["response"]["results"]["result"][0]["useCode"]
+        yearBuilt=page.parsed_response["searchresults"]["response"]["results"]["result"][0]["yearBuilt"]
+        street=page.parsed_response["searchresults"]["response"]["results"]["result"][0]["address"]["street"]
+        return [type,useCode,yearBuilt,street]
+      end 
     else 
       redirect_to '/renters-policies-general'
     end
