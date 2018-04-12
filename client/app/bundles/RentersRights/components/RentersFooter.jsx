f177d06eedc7e9c90ceb96138e39b23752402b47
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-04-12 13:18:49 -0700

    update disclaimer language

--- client/app/bundles/RentersRights/components/RentersFooter.jsx
+++ client/app/bundles/RentersRights/components/RentersFooter.jsx
@@ -47,12 +47,17 @@ export default class RentersFooter extends React.Component {
       <footer
         className="renters-footer"
       >
-        <br/>
-        <br/>
         <div className="content-container">
           <p className="text-center">
-            This app only covers San José, California. This app is not a substitute for legal advice. If you are seeking legal aid, please consult a lawyer immediately. Find a <a href="https://lawhelpca.org">legal aid office</a>.</p>
-          <p className="text-center">© 2017 <a href="http://www.codeforsanjose.com">Code for San José</a></p>
+            <small>
+              Renter's Rights Guide only covers San José, California. This website is for informational purposes only and is not intended to be legal advice or a substitute for San José ordinances. If you are seeking legal aid, please consult a lawyer immediately. Find a <a href="https://lawhelpca.org">legal aid office</a>.
+            </small>
+          </p>
+          <p className="text-center">
+            <small>
+              © <a href="http://www.codeforsanjose.com">Code for San José</a>
+            </small>
+          </p>
         </div>
       </footer>
     )
