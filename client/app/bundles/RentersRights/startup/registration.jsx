c1ba47a97975373a8ff7f0dd277807d368a06fc1
Yan-Yin Choy <yanyinchoy@gmail.com>
2020-01-17 08:45:59 -0800

    remove components from registration. Addresses #90

--- client/app/bundles/RentersRights/startup/registration.jsx
+++ client/app/bundles/RentersRights/startup/registration.jsx
@@ -1,37 +1,7 @@
 import ReactOnRails from 'react-on-rails';
 import Home from '../components/Home';
-import Eviction from '../components/Eviction';
-import About from '../components/About';
-import RentCalculator from '../components/RentCalculator';
-import Resources from '../components/resources/Resources';
-import ReportIssue from '../components/ReportIssue';
-import FindJurisdiction from '../components/FindJurisdiction';
-import RentIncrease from '../components/RentIncrease';
-import ResourceIndexItem from '../components/resources/ResourceIndexItem';
-import HousingDiscrimination from '../components/HousingDiscrimination';
-import MobileHomeOrdinance from '../components/MobileHomeOrdinance';
-import EllisActOrdinance from '../components/EllisActOrdinance';
-import TenantProtectionOrdinance from '../components/TenantProtectionOrdinance';
-
-
-//import { addLocaleData } from 'react-intl';
-//import esLocaleData from 'react-intl/locale-data/es';
-
-//addLocaleData(esLocaleData);
 
 // This is how react_on_rails can see the components in the browser.
 ReactOnRails.register({
-  Home,
-  About,
-  ReportIssue,
-  Eviction,
-  RentCalculator,
-  RentIncrease,
-  Resources,
-  FindJurisdiction,
-  ResourceIndexItem,
-  HousingDiscrimination,
-  MobileHomeOrdinance,
-  EllisActOrdinance,
-  TenantProtectionOrdinance,
+  Home
 });
