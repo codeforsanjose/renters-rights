7c10b6aa678cbe87fe12bcc6a22b86ebf150c4c2
Yan-Yin Choy <yanyinchoy@gmail.com>
2020-01-17 08:33:19 -0800

    update homepage to direct visitors to City of San Jose Housing Department website. Fixes #90

--- client/app/bundles/RentersRights/components/Home.jsx
+++ client/app/bundles/RentersRights/components/Home.jsx
@@ -1,71 +1,5 @@
-{/* Locale set up. Remove comment when app is translated.
-  import { FormattedMessage } from 'react-intl';
-  */}
 import React from 'react';
 import RentersLayout from './RentersLayout'
-
-{/* Locale set up. Remove comment when app is translated.
-
-export default class Home extends React.Component {
-    render () {
-    const { locale } = this.props;
-
-    return (
-      <RentersLayout locale={locale}>
-        <div className="content-container">
-          <div className="page-header">
-            <h1>
-              <FormattedMessage
-                defaultMessage="Helping renters in San José, California understand the laws and programs in place to protect them."
-                id="home.title"
-              />
-            </h1>
-          </div>
-          <br/>
-          <div className="home-links">
-            <div className="col-sm-12 col-md-6">
-              <h1>
-                <FormattedMessage
-                  defaultMessage="How can we help you today?"
-                  id="home.subtitle"
-                />
-              </h1>
-              <div className="list-group">
-                <a href={`/${locale}/eviction`} className="list-group-item list-group-item-danger">
-                  <h4 className="list-group-item-heading">
-                    <FormattedMessage
-                      defaultMessage="I want to learn about tenant eviction."
-                      id="home.eviction"
-                    />
-                  </h4>
-                </a>
-              </div>
-              <div className="list-group">
-                <a href={`/${locale}/renters`} className="list-group-item list-group-item-danger">
-                  <h4 className="list-group-item-heading">
-                    <FormattedMessage
-                      defaultMessage="I want to learn about renter's rights."
-                      id="home.renters"
-                    />
-                  </h4>
-                </a>
-              </div>
-               <div className="list-group">
-
-                <a href={`/${locale}/resources`} className="list-group-item list-group-item-danger">
-                  <h4 className="list-group-item-heading">
-                    <FormattedMessage
-                      defaultMessage="I want to learn about local resources."
-                      id="home.resources"
-                    />
-                  </h4>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </RentersLayout>
-    */}
 
   export default class Home extends React.Component {
    render() {
@@ -75,24 +9,22 @@ export default class Home extends React.Component {
        <RentersLayout>
          <div className="content-container">
            <div className="page-header text-center">
-             <h1>Know Your Rights as a San José, California Renter</h1>
-           </div>
-           <div className="row">
-              <div className=" col-md-12 text-center">
-                <p>
-                  <a href="/eviction" className="btn btn-sq-lg btn-eviction">
-                    <br/><span className="glyphicon glyphicon-file"></span> <br/>Facing Eviction
-                  </a>
-                  <a href="/rent-increase" className="btn btn-sq-lg btn-rent-increase">
-                    <br/><span className="glyphicon glyphicon-signal"></span> <br/>Handling Rent Increase
-                  </a>
-                  <a href="/housing-discrimination/" className="btn btn-sq-lg btn-discrimination">
-                   <br/><span className="glyphicon glyphicon-home"></span> <br/> Facing Discrimination
-                  </a>
-                </p>
-              </div>
+             <p>The Code for San Jose's Renter's Rights Guide website is now archived. Our volunteers started the project in 2017, conducting user and stakeholder research to build a user-friendly website for tenants to learn about their rights.</p>
+             <p>From 2017-2019, we maintained the website, keeping it up to date with the latest policy changes from the City of San José. </p>
+             <p>In January 2020, we decided to archive the website, because the City of San José Housing Department has improved their website user interface</p>
+             <p>To learn about San Jose renter's rights, please visit the <a href="http://sanjoseca.gov/rent" rel="noopener">City of San Jose Housing Department's website</a></p>
+             <p>Learn more about <a href="https://www.codeforsanjose.com/#projects" rel="noopener">Code for San José projects</a></p>
            </div>
          </div>
+         <footer className="renters-footer">
+          <div className="content-container">
+            <p className="text-center">
+              <small>
+                Made by <a href="http://www.codeforsanjose.com">Code for San José</a>
+              </small>
+            </p>
+          </div>
+        </footer>
        </RentersLayout>
      )
    }
--- client/app/bundles/RentersRights/components/RentersLayout.jsx
+++ client/app/bundles/RentersRights/components/RentersLayout.jsx
@@ -1,55 +1,18 @@
-//import { IntlProvider } from 'react-intl';
-//import messages from '../messages.json';
 import React from 'react';
-import RentersFooter from './RentersFooter';
 import RentersHeader from './RentersHeader';
 
-/* Locale set up. Remove uncomment part when app translation is ready.
-export default function RentersLayout({
-  availableLocales,
-  children,
-  className,
-  locale,
-}) {
-  return (
-    <IntlProvider
-      key={locale}
-      locale={locale}
-      messages={messages[locale]}
-    >
-      <div
-        className={className}
-      >
-        <RentersHeader
-          availableLocales={availableLocales}
-          locale={locale}
-        />
-        <div
-          className="container"
-        >
-          {children}
-        </div>
-        <RentersFooter />
-      </div>
-    </IntlProvider>
-  );
-}
-*/
 export default function RentersLayout({
   children,
   className
 }) {
   return (
-      <div
-        className={className}
-      >
+      <div className={className}>
         <RentersHeader/>
         <div
           className="container"
         >
           {children}
         </div>
-        <RentersFooter />
       </div>
   );
 }
