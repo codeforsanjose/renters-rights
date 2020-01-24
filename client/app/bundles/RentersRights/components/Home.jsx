805d8fdbd58ed2e07a1b3488e27da995d4216d44
Yan-Yin  Choy <yanyinchoy@gmail.com>
2020-01-24 10:04:22 -0800

    Update project description

--- client/app/bundles/RentersRights/components/Home.jsx
+++ client/app/bundles/RentersRights/components/Home.jsx
@@ -11,7 +11,8 @@ import RentersLayout from './RentersLayout'
            <div className="page-header text-center">
              <p>The Code for San Jose's Renter's Rights Guide website is now archived. Our volunteers started the project in 2017, conducting user and stakeholder research to build a user-friendly website for tenants to learn about their rights.</p>
              <p>From 2017-2019, we built and maintained the website, keeping it up to date with the latest policy changes from the City of San José. </p>
-             <p>In January 2020, we decided to archive the website, because the City of San José Housing Department has improved their website user interface</p>
+             <p>In early January 2020, we decided to archive the original website, because the City of San José Housing Department has improved their website user interface</p>
+             <p>We are now collaborating with Stanford Legal Design Lab students to build tools that help tenants facing eviction.</p>
              <p>To learn about San Jose renter's rights, please visit the <a href="http://sanjoseca.gov/rent" rel="noopener">City of San Jose Housing Department's website</a></p>
              <p>Learn more about <a href="https://www.codeforsanjose.com/#projects" rel="noopener">Code for San José projects</a></p>
            </div>
